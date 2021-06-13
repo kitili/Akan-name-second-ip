bd5c43922b2f1944df99669f9b6a2ddc09bd6384
your_username <your_email_address@example.com>
2021-06-13 21:59:18 +0300

    parseint

--- css/js/script.js
+++ css/js/script.js
@@ -86,3 +86,21 @@ function leapYear() {
         return false;
     }
 }
+
+function formData() {
+    var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var association = ["Universe", "Peace", "Ocean", "Spider", "Earth", "Fertility", "God"];
+    var cc = parseInt(document.getElementById("century").value.slice(0, 2));
+    var yy = parseInt(document.getElementById("year").value.slice(2, 4));
+    var dd = parseInt(document.getElementById("day").value);
+    var mm = parseInt(document.getElementById("month").value);
+    var method = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+    var weekDay = daysOfTheWeek[method.toFixed()];
+    var gender = parseInt(document.getElementById("gender").value);
+
+    if (weekDay === 'Sunday' && gender == 2) {
+        document.getElementById("output").value = "You were born on Sunday: Your Akan Name is: " + akanMale[0] + ".It is associated with: " + association[0];
+        return true;
+    }
