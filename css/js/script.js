e39f34ed9e4c7505212c6beba9c57d8572c930b5
your_username <your_email_address@example.com>
2021-06-13 20:58:25 +0300

    return

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,9 @@
-function formData() {
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+
+function date() {
+    var date = document.getElementById("date").value;
+    if (date == "") {
+        alert("Please fill in the Date ");
+        return false;
+    }
