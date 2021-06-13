6e2078136bad44ff1221ffdf8b2d80ffe2fe3aae
your_username <your_email_address@example.com>
2021-06-13 21:50:36 +0300

    return

--- css/js/script.js
+++ css/js/script.js
@@ -13,4 +13,30 @@ function month() {
     alert("PLease fill in the Month");
     return false;
 }
+
+function showYear() {
+    var year = document.getElementById("year").value;
+
+    if (year == "") {
+        alert("PLease fill in the Year");
+        return false;
+    }
+}
+
+}
+
+function century() {
+    var century = document.getElementById("century").value;
+    if (century == "") {
+        alert("Please fill in the Century");
+        return false;
+    }
+}
+
+function gender() {
+    var gender = document.getElementById("gender").value;
+    if (gender == "1") {
+        alert("Please select gender");
+        return false;
+    }
 }
