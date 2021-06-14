83813f197d72f58225eb016f788f8a3b655b142b
your_username <your_email_address@example.com>
2021-06-14 12:09:34 +0300

    changes to css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 function submition() {
     /* declaration  and initialisation */
-<<<<<<< HEAD
     let month = document.getElementById("month").value;
     let mm = parseInt(month);
     let day = document.getElementById("day").value;
@@ -8,15 +7,6 @@ function submition() {
     let year = document.getElementById("year").value;
     let yy = parseInt(year);
     let cc = (yy - 1) / 100 + 1;
-=======
-    var month = document.getElementById("month").value;
-    var mm = parseInt(month);
-    var day = document.getElementById("day").value;
-    var dd = parseInt(day);
-    var year = document.getElementById("year").value;
-    var yy = parseInt(year);
-    var cc = (yy - 1) / 100 + 1;
->>>>>>> gh-pages
     /* the formula */
     var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
     var boyNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -60,9 +50,5 @@ function submition() {
         document.getElementById("display").innerHTML = ("you were born on Sunday,your akan name is " + girlNames[0]);
     } else {
         alert("input data please");
-<<<<<<< HEAD
     }
 }
-=======
-    }
->>>>>>> gh-pages
