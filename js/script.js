5b65bea2fd82226080eed5f7a1fd42d05157a199
your_username <your_email_address@example.com>
2021-06-14 13:37:05 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function submition() {
     /* declaration  and initialisation */
-    let month = document.getElementById("month").value;
-    let mm = parseInt(month);
-    let day = document.getElementById("day").value;
-    let dd = parseInt(day);
-    let year = document.getElementById("year").value;
-    let yy = parseInt(year);
-    let cc = (yy - 1) / 100 + 1;
+    var month = document.getElementById("month").value;
+    var mm = parseInt(month);
+    var day = document.getElementById("day").value;
+    var dd = parseInt(day);
+    var year = document.getElementById("year").value;
+    var yy = parseInt(year);
+    var cc = (yy - 1) / 100 + 1;
     /* the formula */
     var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
     var boyNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
